06a3daf164bb1a7d35c45b712a326e07c68720e6
LittleRuiRui <alexji1992@gmail.com>
2026-08-29 00:58:26 +0800

    Deploy site at peacedskin.com

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 const buildVersion = process.env.GITHUB_SHA || `${Date.now()}`;
 
 export default defineConfig({
-  base: "/skincare101/",
+  base: "/",
   plugins: [react()],
   define: {
     __BUILD_VERSION__: JSON.stringify(buildVersion),
