f7b18d30f2be87ed218dcc779ef43f575abcdaf4
LittleRuiRui <alexji1992@gmail.com>
2026-08-28 08:30:51 +0800

    wrap app with site-wide language guard

---
+++ src/V6App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import V5App from "./V5App";
+import LanguageConsistencyGuard from "./components/LanguageConsistencyGuard";
+
+export default function V6App(){
+ return <><V5App/><LanguageConsistencyGuard/></>;
+}
